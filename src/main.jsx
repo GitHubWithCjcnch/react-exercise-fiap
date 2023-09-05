88f4bad5f6305a9e5e4fd49df99454c0464e6543
Caio Vitor <githubwithcjcnch@gmail.com>
2023-09-04 22:27:36 -0300

    Adding the routes to the application

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 import App from './App.jsx'
+import Error from './routes/Error/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+import Home from './routes/Home/index.jsx'
+import EditarProduto from './routes/EditarProdutos/index.jsx'
 import './index.css'
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/produtos",
+        element: <Produtos />
+      },
+      {
+        path: "/produtos/editar/:id",
+        element: <EditarProduto />
+      },
+      {
+        path: "/antiga",
+        element: <Navigate to="/"/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </React.StrictMode>,
 )
