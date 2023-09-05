fc6a6344908388e78cef667d9de1a571000a767f
Caio Vitor <githubwithcjcnch@gmail.com>
2023-09-04 22:26:13 -0300

    Adding the Menu, Outlet and Rodape components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Outlet } from 'react-router-dom'
+import  Menu  from './components/Menu/Menu'
+import  Rodape  from './components/Rodape/Rodape'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      a
+      <Menu />
+      <Outlet />
+      <Rodape />
     </>
   )
 }
